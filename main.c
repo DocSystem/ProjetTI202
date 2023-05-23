5e2d099b96962675828ff0d09ae93c0d0d1402c4
DocSystem <42613513+DocSystem@users.noreply.github.com>
2023-05-23 17:59:01 +0200

    Continue adding commands

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include "chained_list.h"
 #include "area.h"
 
-void execCmd(int* error, Area* area) {
+void execCmd(int* error, Area* area, int* MODE) {
     // split cmd into two variables
     // first variable is the command
     // second variable is an array of arguments
@@ -15,6 +15,8 @@ void execCmd(int* error, Area* area) {
     Command command = parseCommand(cmd);
     *error = 0;
     if (strcmp(command.command, "clear") == 0) {
+        *MODE = -1;
+        clearScreen();
         // clear
     }
     else if (strcmp(command.command, "exit") == 0) {
@@ -43,11 +45,115 @@ void execCmd(int* error, Area* area) {
         int y1 = parseInt(command.args[1]);
         int x2 = parseInt(command.args[2]);
         int y2 = parseInt(command.args[3]);
+        if (x1 < 0 || x1 >= area->width - 1 || x2 < 0 || x2 >= area->width - 1 || y1 < 0 || y1 >= area->height - 2 || y2 < 0 || y2 >= area->height - 2) {
+            *error = 1;
+            return;
+        }
         Point* p1 = createPoint(x1, y1);
         Point* p2 = createPoint(x2, y2);
         Shape* l = createLineShape(p1, p2);
         // add line to area
         add_shape_to_area(area, l);
+    }
+    else if (strcmp(command.command, "circle") == 0) {
+        // circle <x> <y> <radius>
+        if (command.arg_count != 3) {
+            *error = 1;
+            return;
+        }
+        int x = parseInt(command.args[0]);
+        int y = parseInt(command.args[1]);
+        int radius = parseInt(command.args[2]);
+        if (x < 0 || x >= area->width - 1 || y < 0 || y >= area->height - 2) {
+            *error = 1;
+            return;
+        }
+        Point* p = createPoint(x, y);
+        Shape* c = createCircleShape(p, radius);
+        // add circle to area
+        add_shape_to_area(area, c);
+    }
+    else if (strcmp(command.command, "square") == 0) {
+        // square <x> <y> <length>
+        if (command.arg_count != 3) {
+            *error = 1;
+            return;
+        }
+        int x = parseInt(command.args[0]);
+        int y = parseInt(command.args[1]);
+        int length = parseInt(command.args[2]);
+        if (x < 0 || x >= area->width - 1 || y < 0 || y >= area->height - 2) {
+            *error = 1;
+            return;
+        }
+        Point* p = createPoint(x, y);
+        Shape* s = createSquareShape(p, length);
+        // add square to area
+        add_shape_to_area(area, s);
+    }
+    else if (strcmp(command.command, "rectangle") == 0) {
+        // rectangle <x> <y> <width> <height>
+        if (command.arg_count != 4) {
+            *error = 1;
+            return;
+        }
+        int x = parseInt(command.args[0]);
+        int y = parseInt(command.args[1]);
+        int width = parseInt(command.args[2]);
+        int height = parseInt(command.args[3]);
+        if (x < 0 || x >= area->width - 1 || y < 0 || y >= area->height - 2) {
+            *error = 1;
+            return;
+        }
+        Point* p = createPoint(x, y);
+        Shape* r = createRectangleShape(p, width, height);
+        // add rectangle to area
+        add_shape_to_area(area, r);
+    }
+    else if (strcmp(command.command, "polygon") == 0) {
+        // polygon <x1> <y1> <x2> <y2> ... <xn> <yn>
+        if (command.arg_count < 6 || command.arg_count % 2 != 0) {
+            *error = 1;
+            return;
+        }
+        int* x = malloc(sizeof(int) * command.arg_count / 2);
+        int* y = malloc(sizeof(int) * command.arg_count / 2);
+        for (int i = 0; i < command.arg_count; i++) {
+            if (i % 2 == 0) {
+                x[i / 2] = parseInt(command.args[i]);
+            }
+            else {
+                y[i / 2] = parseInt(command.args[i]);
+            }
+        }
+        for (int i = 0; i < command.arg_count / 2; i++) {
+            if (x[i] < 0 || x[i] >= area->width - 1 || y[i] < 0 || y[i] >= area->height - 2) {
+                *error = 1;
+                return;
+            }
+        }
+        Point** points = malloc(sizeof(Point*) * command.arg_count / 2);
+        for (int i = 0; i < command.arg_count / 2; i++) {
+            points[i] = createPoint(x[i], y[i]);
+        }
+        Shape* p = createPolygonShape(points, command.arg_count / 2);
+        // add polygon to area
+        add_shape_to_area(area, p);
+    }
+    else if (strcmp(command.command, "plot") == 0) {
+        *MODE = 0;
+    }
+    else if (strcmp(command.command, "list") == 0) {
+        *MODE = 1;
+    }
+    else if (strcmp(command.command, "delete") == 0) {
+        // delete <id>
+    }
+    else if (strcmp(command.command, "erase") == 0) {
+        erase_area(area);
+    }
+    else if (strcmp(command.command, "help") == 0) {
+        *MODE = 2;
     }
     else {
         *error = 1;
@@ -67,21 +173,48 @@ int main() {
     */
     //showWindowBox();
 
-    Area* drawingArea = create_area(100, 100);
+    int MODE = 0;
+
+    win_size size = getWindowSize();
+
+    Area* drawingArea = create_area(size.width, size.height - 1);
     clear_area(drawingArea);
 
     int error = 0;
 
     clearScreen();
     while (1) {
-        win_size size = getWindowSize();
-        showWindowBox();
+        // showWindowBox();
         // render shapes
-        draw_area(drawingArea);
+        clearScreen();
+        switch (MODE) {
+            case 0:
+                // show plot (default / command `plot`)
+                draw_area(drawingArea);
+                print_area(drawingArea);
+                break;
+
+            case 1:
+                // show points list (command `list`)
+                break;
+
+            case 2:
+                // show help (command `help`)
+                printHelp();
+                break;
+
+            case -1:
+                // show nothing (screen cleared)
+                break;
+
+            default:
+                MODE = 0;
+                break;
+        }
         if (error == 1) {
             printAtCoos(size.width - 1, size.height, "❌");
         }
-        execCmd(&error, drawingArea);
+        execCmd(&error, drawingArea, &MODE);
     }
 
     return 0;
